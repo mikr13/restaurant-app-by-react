373d8070e19dbdaf99e1eaa71e0b4ee3b293be0c
Mihir Kumar <mihirpandey.13@gmail.com>
2018-10-05 12:08:26 +0530

    ConFusion update 1 render comment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+
 import './App.css';
 
+import Menu from './components/MenuComponent';
+
+import { DISHES } from './shared/dishes';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Ristorante Con Fusion",
+      dishes: DISHES
+    };
+  };
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
+        <Navbar dark color="primary" className="navbar">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">{this.state.title}</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+  formatDate(string){
+    var options = { month: 'short', day: 'numeric', year: 'numeric' };
+    return new Date(string).toLocaleDateString([],options);
+  }
+
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else
+      return (
+        <div></div>
+      );
+  }
+
+  renderComments(comments) {
+    if ( comments !=null )
+      return(
+        <ul className="list-unstyled">
+          { comments.map((comment) => (
+              <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>-- {comment.author}, {this.formatDate(comment.date)}</p>
+              </li>
+            ))
+          }
+        </ul>
+      );
+    else
+      return (
+      <div></div>
+      );
+  }
+
+  render() {
+    if(this.props.dish != null)
+      return (
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 m-1">
+            <Card>
+              <CardBody>
+                <CardTitle>Comments</CardTitle>
+                {this.renderComments(this.props.dish.comments)}
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      );
+    else
+      return (
+        <div></div>
+      );
+  }
+}
+
+export default DishDetail;
