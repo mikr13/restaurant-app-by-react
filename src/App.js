2271db6ee627357ac0e50fb77166d54590f2d92a
Mihir Kumar <mihirpandey.13@gmail.com>
2018-10-16 12:59:46 +0530

    conFusion update 4 Single Page Application update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 
@@ -18,9 +19,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+    </BrowserRouter>
     );
   }
 }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function FormatDate({cdate}) {
   var options = { month: 'short', day: '2-digit', year: 'numeric' };
@@ -46,17 +47,30 @@ const DishDetail = (props) => {
 
   if(props.dish != null)
     return (
-      <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-6 ml-4 mr-2 my-3">
-          <RenderDish dish= {props.dish} />
+      <div className="container">
+        <div className="row" style={{margin: 0 + 'em', padding: 0 + 'em'}}>
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
+          </div>                
         </div>
-        <div className="col-xs-12 col-sm-12 col-md-5 my-3">
-          <Card>
-            <CardBody>
-              <CardTitle>Comments</CardTitle>
-              <RenderComments comments= {props.dish.comments} />
-            </CardBody>
-          </Card>
+        <div className="row" style={{margin: 0 + 'em', padding: 0 + 'em'}}>
+          <div className="col-xs-12 col-sm-12 col-md-6 ml-4 mr-2 my-3">
+            <RenderDish dish= {props.dish} />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-5 my-3">
+            <Card>
+              <CardBody>
+                <CardTitle>Comments</CardTitle>
+                <RenderComments comments= {props.comments} />
+              </CardBody>
+            </Card>
+          </div>
         </div>
       </div>
     );
